d4ed3276f62d66b64f0ecea701ed5101805f11d6
msrocka <michael.srocka@gmail.com>
2025-10-01 14:44:53 +0200

    example for only extracting the numeric variables

--- src/test/java/org/openlca/sd/SimulatorExample.java
+++ src/test/java/org/openlca/sd/SimulatorExample.java
@@ -2,9 +2,8 @@ package org.openlca.sd;
 
 import java.io.File;
 
-import org.openlca.sd.eqn.Id;
 import org.openlca.sd.eqn.Simulator;
-import org.openlca.sd.util.TensorPrinter;
+import org.openlca.sd.eqn.Vars;
 import org.openlca.sd.xmile.Xmile;
 
 public class SimulatorExample {
@@ -12,16 +11,20 @@ public class SimulatorExample {
 	public static void main(String[] args) {
 		var xmile = Xmile.readFrom(new File("examples/simple.stmx"));
 		var sim = Simulator.of(xmile).orElseThrow();
+		var vars = Vars.readFrom(xmile).orElseThrow();
 
-		var prnt = new TensorPrinter();
-		var stock = Id.of("Natural\\nResources");
 		sim.forEach(res -> {
 			if (res.hasError()) {
 				System.out.println("error: " + res.error());
 			} else {
 				var state = res.value();
-				var val = state.valueOf(stock).orElseThrow();
-				prnt.print(val);
+				System.out.println("Iteration: " + state.iteration());
+				for (var v : vars) {
+					var varVal = state.valueOf(v.name()).orElseThrow();
+					if (varVal.isNumCell()) {
+						System.out.printf("  %s : %.2f%n", v.name().label(), varVal.asNum());
+					}
+				}
 			}
 		});
 	}
